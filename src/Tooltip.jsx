a2455e52529a52b9d8d1c5c3dc09d280c7c6d7e5
prakashghorpade <prakashghorpade2001@gmail.com>
2024-07-13 15:27:22 +0530

    First commit

---
+++ src/Tooltip.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useState } from 'react';
+
+const Tooltip = (props) => {
+
+
+  const [ishovering, setIshovering] = useState(true);
+
+  return (
+
+    <>
+      <div className='main'>
+
+        <div className='box'>
+          <div className='tooltiptext' onMouseLeave={() => setIshovering(false)} onMouseEnter={() => setIshovering(true)}>
+            <p> Hover over me!</p>
+          </div>
+
+          {
+            ishovering && <>
+              <div className={props.value} >
+                <p> Thanks for hovering! I'am tooltip</p>
+              </div>
+            </>
+
+          }
+        </div>
+
+
+        <div>
+          <h2>This is the Toolkit Project By the Prakash Ghorpade</h2>
+        </div>
+
+      </div>
+    </>
+
+  )
+}
+
+export default Tooltip
+
